09a31b9d3a742604cf10fbe4dd0f9c04166578f9
Robin Kåveland <robin@kaveland.no>
2025-11-02 15:19:22 +0100

    Split into import/serve subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::entur_data::append_data;
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use duckdb::Connection;
 use reqwest;
 use reqwest::Client;
 use tracing::info;
@@ -12,48 +13,64 @@ mod entur_data;
 mod entur_siriformat;
 
 #[derive(Parser)]
-#[command(name = "forsinka")]
-#[command(about = "Run API to expose current public transit delays in Norway")]
-#[command(version = env!("CARGO_PKG_VERSION"))]
-struct Forsinka {
+struct SharedOptions {
     /// Instead of connecting to Entur API, use a static json data file that has been downloaded
     #[arg(short = 's', long = "static-data")]
     static_data: Option<String>,
-    /// Host the webapp on this particular port
-    #[arg(short = 'p', long = "port", default_value = "4500")]
-    port: u16,
     /// Retrieve the data from an alternate source. The source must deliver json-data that matches
     /// the Entur API.
     #[arg(short = 'u', long = "api-url", default_value = entur_data::ENTUR_API_URL)]
     api_url: String,
-    /// Check for new data every fetch-interval seconds. If not provided, never refetch.
-    #[arg(short = 'i', long = "fetch-interval-seconds")]
-    fetch_interval_seconds: Option<u16>,
-    /// DuckDB to connect to - uses an inmemory-db if not configured.
-    #[arg(short = 'd', long = "db-url")]
-    db_url: Option<String>,
     /// URL or file path to fetch quays.parquet and stops.parquet for geolocating stops
     #[arg(
         long = "parquet-root",
         default_value = "https://kaaveland-bus-eta-data.hel1.your-objectstorage.com/"
     )]
     parquet_root: String,
+    /// DuckDB to connect to - uses an inmemory-db if not configured.
+    #[arg(short = 'd', long = "db-url")]
+    db_url: Option<String>,
     /// requestorId to send to entur api, to fetch only diff since last fetch
     /// The default behaviour is to generate a unique one startup to receive the full dataset the first run
     #[arg(long = "requestor-id")]
     requestor_id: Option<String>,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::registry()
-        .with(fmt::layer().with_writer(std::io::stderr))
-        .with(EnvFilter::try_from_env("RUST_LOG").unwrap_or_else(|_| EnvFilter::new("info")))
-        .init();
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a local DuckDB file with imported data from entur
+    ///
+    /// This is convenient to explore the data
+    Import {
+        #[command(flatten)]
+        shared_options: SharedOptions,
+    },
+    /// Start a long-lived http server that continually imports data
+    Serve {
+        #[command(flatten)]
+        shared_options: SharedOptions,
+        /// Host the webapp on this particular port
+        #[arg(short = 'p', long = "port", default_value = "4500")]
+        port: u16,
+        /// Check for new data every fetch-interval seconds. If not provided, never refetch.
+        #[arg(short = 'i', long = "fetch-interval-seconds")]
+        fetch_interval_seconds: Option<u16>,
+    },
+}
 
-    let args = Forsinka::try_parse()?;
+#[derive(Parser)]
+#[command(name = "forsinka")]
+#[command(about = "Run API to expose current public transit delays in Norway")]
+#[command(version = env!("CARGO_PKG_VERSION"))]
+struct Forsinka {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+async fn initial_import(args: &SharedOptions) -> anyhow::Result<Connection> {
     let me = args
         .requestor_id
+        .clone()
         .unwrap_or_else(|| Uuid::new_v4().to_string());
 
     let client = Client::new();
@@ -66,6 +83,30 @@ async fn main() -> anyhow::Result<()> {
     info!("Start inserting journeys");
     append_data(vehicle_journeys, &db)?;
     info!("Load initial SIRI-et data into DuckDB");
+    Ok(db)
+}
 
-    Ok(())
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_writer(std::io::stderr))
+        .with(EnvFilter::try_from_env("RUST_LOG").unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
+
+    let args = Forsinka::try_parse()?;
+
+    match args.command {
+        Commands::Import { shared_options } => {
+            let _db = initial_import(&shared_options).await?;
+            Ok(())
+        }
+        Commands::Serve {
+            shared_options,
+            port,
+            fetch_interval_seconds,
+        } => {
+            let _db = initial_import(&shared_options).await?;
+            Ok(())
+        }
+    }
 }
