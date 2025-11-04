117200d483dbb0686456e3943af9dec30c4bd42d
Robin Kåveland <robin@kaveland.no>
2025-11-04 09:30:43 +0100

    Age out old data

--- src/db.rs
+++ src/db.rs
@@ -3,13 +3,37 @@ use duckdb::Connection;
 use std::time::Instant;
 use tracing::{info, instrument};
 
-pub fn prepare_db(db_url: &Option<String>, parquet_root: &str) -> anyhow::Result<Connection> {
+const STOP_DATA: &str = "
+create or replace table stopdata as
+from stops s
+  join quays q on q.stopPlaceRef = s.id
+select
+  s.name as name,
+  q.id as stop_point_ref,
+  coalesce(q.location_latitude, s.location_latitude) as lat,
+  coalesce(q.location_longitude, s.location_longitude) as lon
+";
+
+pub fn prepare_db(
+    db_url: &Option<String>,
+    parquet_root: &str,
+    threads: u8,
+    memory_gb: u8,
+) -> anyhow::Result<Connection> {
     info!("Prepare database {:?}", db_url);
 
     let db = match db_url {
         None => Connection::open_in_memory(),
         Some(f) => Connection::open(f.as_str()),
     }?;
+
+    db.execute_batch(
+        format!(
+            "set memory_limit = '{}GB'; set threads = {}",
+            memory_gb, threads
+        )
+        .as_str(),
+    )?;
 
     let schema: &str = include_str!("schema.sql");
 
@@ -26,6 +50,7 @@ pub fn prepare_db(db_url: &Option<String>, parquet_root: &str) -> anyhow::Result
         "create or replace table stops as from read_parquet($1);",
         [stops.as_str()],
     )?;
+    db.execute_batch(STOP_DATA)?;
 
     Ok(db)
 }
@@ -33,8 +58,8 @@ pub fn prepare_db(db_url: &Option<String>, parquet_root: &str) -> anyhow::Result
 const WITH_CURRENT: &str = "
 with current as (
   from vehicle_journey
-  select distinct on(vehicle_journey_id) vehicle_journey_id, recorded_at_time
-  order by recorded_at_time desc
+  select distinct on(vehicle_journey_id) vehicle_journey_id, version, recorded_at_time
+  order by version desc, recorded_at_time desc
 )
 ";
 
@@ -52,27 +77,30 @@ pub fn replace_data(
         tx.appender("recorded_call")?,
     )?;
 
-    // TODO: Check this logic / timestamps - requires diving into the data. We may need more IDs to actually do this successfully.
     tx.execute_batch(
-        format!(
-            "create or replace table estimated_call as {WITH_CURRENT} from estimated_call join current using(vehicle_journey_id, recorded_at_time);"
-        )
-        .as_str(),
+        "delete from vehicle_journey where finished and age(recorded_at_time) > interval 12 hours;",
     )?;
 
     tx.execute_batch(
-        format!(
-            "create or replace table recorded_call as {WITH_CURRENT} from recorded_call join current using(vehicle_journey_id, recorded_at_time);"
-        )
-        .as_str(),
-    )?;
-
-    tx.execute_batch(
-        format!(
-            "create or replace table vehicle_journey as {WITH_CURRENT} from vehicle_journey join current using(vehicle_journey_id, recorded_at_time);"
-        )
+            format!(
+                "create or replace table estimated_call as {WITH_CURRENT} from estimated_call join current using(vehicle_journey_id, version, recorded_at_time);"
+            )
             .as_str(),
-    )?;
+        )?;
+
+    tx.execute_batch(
+            format!(
+                "create or replace table recorded_call as {WITH_CURRENT} from recorded_call join current using(vehicle_journey_id, version, recorded_at_time);"
+            )
+            .as_str(),
+        )?;
+
+    tx.execute_batch(
+            format!(
+                "create or replace table vehicle_journey as {WITH_CURRENT} from vehicle_journey join current using(vehicle_journey_id, version, recorded_at_time);"
+            )
+                .as_str(),
+        )?;
 
     tx.commit()?;
     tracing::Span::current().record("duration_ms", start.elapsed().as_millis());
--- src/entur_data.rs
+++ src/entur_data.rs
@@ -99,6 +99,7 @@ pub trait RowType {
     /// Maps 1-1 with a DuckDB-table and can be inserted with the Appender API for greater throughput.
     /// NB! It is important to match column positions correctly for this to work.
     fn append_with(self, appender: &mut Appender) -> duckdb::Result<()>;
+    #[allow(unused)]
     fn map_from(row: &duckdb::Row) -> duckdb::Result<Self>
     where
         Self: Sized;
@@ -108,6 +109,7 @@ pub trait RowType {
 #[derive(Clone, Debug, PartialEq)]
 pub struct VehicleJourneyRow {
     pub id: String,
+    pub version: u32,
     pub data_source: String,
     pub recorded_at_time: DateTime<FixedOffset>,
     pub cancellation: bool,
@@ -116,8 +118,10 @@ pub struct VehicleJourneyRow {
     pub direction_ref: String,
     pub destination_ref: Option<String>,
     pub origin_ref: Option<String>,
+    pub started: bool,
+    pub finished: bool,
 }
-
+#[allow(unused)]
 fn timestamptz(micros: i64) -> DateTime<FixedOffset> {
     // TODO: Preserve original timezone
     Utc.timestamp_micros(micros)
@@ -126,6 +130,7 @@ fn timestamptz(micros: i64) -> DateTime<FixedOffset> {
         .into()
 }
 
+#[allow(unused)]
 fn optional_timestamptz(col: Option<i64>) -> Option<DateTime<FixedOffset>> {
     col.map(timestamptz)
 }
@@ -134,6 +139,7 @@ impl RowType for VehicleJourneyRow {
     fn append_with(self, appender: &mut Appender) -> duckdb::Result<()> {
         appender.append_row(duckdb::params![
             self.id,
+            self.version,
             self.data_source,
             self.recorded_at_time,
             self.cancellation,
@@ -141,7 +147,9 @@ impl RowType for VehicleJourneyRow {
             self.line_ref,
             self.direction_ref,
             self.destination_ref,
-            self.origin_ref
+            self.origin_ref,
+            self.started,
+            self.finished
         ])
     }
 
@@ -151,14 +159,17 @@ impl RowType for VehicleJourneyRow {
     {
         Ok(Self {
             id: row.get(0)?,
-            data_source: row.get(1)?,
-            recorded_at_time: timestamptz(row.get(2)?),
-            cancellation: row.get(3)?,
-            extra_journey: row.get(4)?,
-            line_ref: row.get(5)?,
-            direction_ref: row.get(6)?,
-            destination_ref: row.get(7)?,
-            origin_ref: row.get(8)?,
+            version: row.get(1)?,
+            data_source: row.get(2)?,
+            recorded_at_time: timestamptz(row.get(3)?),
+            cancellation: row.get(4)?,
+            extra_journey: row.get(5)?,
+            line_ref: row.get(6)?,
+            direction_ref: row.get(7)?,
+            destination_ref: row.get(8)?,
+            origin_ref: row.get(9)?,
+            started: row.get(10)?,
+            finished: row.get(11)?,
         })
     }
 }
@@ -167,6 +178,7 @@ impl RowType for VehicleJourneyRow {
 #[derive(Clone, Debug, PartialEq)]
 pub struct EstimatedCallRow {
     pub vehicle_journey_id: String,
+    pub version: u32,
     pub data_source: String,
     pub recorded_at_time: DateTime<FixedOffset>,
     pub order: u16,
@@ -182,6 +194,7 @@ impl RowType for EstimatedCallRow {
     fn append_with(self, appender: &mut Appender) -> duckdb::Result<()> {
         appender.append_row(duckdb::params![
             self.vehicle_journey_id,
+            self.version,
             self.data_source,
             self.recorded_at_time,
             self.order,
@@ -200,15 +213,16 @@ impl RowType for EstimatedCallRow {
     {
         Ok(Self {
             vehicle_journey_id: row.get(0)?,
-            data_source: row.get(1)?,
-            recorded_at_time: timestamptz(row.get(2)?),
-            order: row.get(3)?,
-            aimed_arrival_time: optional_timestamptz(row.get(4)?),
-            aimed_departure_time: optional_timestamptz(row.get(5)?),
-            expected_arrival_time: optional_timestamptz(row.get(6)?),
-            expected_departure_time: optional_timestamptz(row.get(7)?),
-            cancellation: row.get(8)?,
-            stop_point_ref: row.get(9)?,
+            version: row.get(1)?,
+            data_source: row.get(2)?,
+            recorded_at_time: timestamptz(row.get(3)?),
+            order: row.get(4)?,
+            aimed_arrival_time: optional_timestamptz(row.get(5)?),
+            aimed_departure_time: optional_timestamptz(row.get(6)?),
+            expected_arrival_time: optional_timestamptz(row.get(7)?),
+            expected_departure_time: optional_timestamptz(row.get(8)?),
+            cancellation: row.get(9)?,
+            stop_point_ref: row.get(10)?,
         })
     }
 }
@@ -217,6 +231,7 @@ impl RowType for EstimatedCallRow {
 #[derive(Clone, Debug, PartialEq)]
 pub struct RecordedCallRow {
     pub vehicle_journey_id: String,
+    pub version: u32,
     pub data_source: String,
     pub recorded_at_time: DateTime<FixedOffset>,
     pub order: u16,
@@ -232,6 +247,7 @@ impl RowType for RecordedCallRow {
     fn append_with(self, appender: &mut Appender) -> duckdb::Result<()> {
         appender.append_row(duckdb::params![
             self.vehicle_journey_id,
+            self.version,
             self.data_source,
             self.recorded_at_time,
             self.order,
@@ -250,15 +266,16 @@ impl RowType for RecordedCallRow {
     {
         Ok(Self {
             vehicle_journey_id: row.get(0)?,
-            data_source: row.get(1)?,
-            recorded_at_time: timestamptz(row.get(2)?),
-            order: row.get(3)?,
-            aimed_arrival_time: optional_timestamptz(row.get(4)?),
-            aimed_departure_time: optional_timestamptz(row.get(5)?),
-            actual_arrival_time: optional_timestamptz(row.get(6)?),
-            actual_departure_time: optional_timestamptz(row.get(7)?),
-            cancellation: row.get(8)?,
-            stop_point_ref: row.get(9)?,
+            version: row.get(1)?,
+            data_source: row.get(2)?,
+            recorded_at_time: timestamptz(row.get(3)?),
+            order: row.get(4)?,
+            aimed_arrival_time: optional_timestamptz(row.get(5)?),
+            aimed_departure_time: optional_timestamptz(row.get(6)?),
+            actual_arrival_time: optional_timestamptz(row.get(7)?),
+            actual_departure_time: optional_timestamptz(row.get(8)?),
+            cancellation: row.get(9)?,
+            stop_point_ref: row.get(10)?,
         })
     }
 }
@@ -270,7 +287,7 @@ pub struct VehicleJourneyAppend {
 }
 
 impl VehicleJourneyAppend {
-    fn new(journey: EstimatedVehicleJourney) -> Option<Self> {
+    fn new(journey: EstimatedVehicleJourney, version: u32) -> Option<Self> {
         // This is long for an annoying technical reason - we want to move out from `journey`,
         // and can only do so once, so we can't do the obvious thing and split it into three functions
         // with one for each row type
@@ -295,6 +312,7 @@ impl VehicleJourneyAppend {
             .into_iter()
             .map(|et| EstimatedCallRow {
                 vehicle_journey_id: id.clone(),
+                version,
                 data_source: data_source.clone(),
                 recorded_at_time: recorded_at_time.clone(),
                 order: et.order,
@@ -314,6 +332,7 @@ impl VehicleJourneyAppend {
             .into_iter()
             .map(|rc| RecordedCallRow {
                 vehicle_journey_id: id.clone(),
+                version,
                 data_source: data_source.clone(),
                 recorded_at_time,
                 order: rc.order,
@@ -328,6 +347,7 @@ impl VehicleJourneyAppend {
 
         let vehicle_journey_row = VehicleJourneyRow {
             id,
+            version,
             data_source,
             recorded_at_time,
             cancellation: journey.cancellation.unwrap_or(false),
@@ -362,6 +382,13 @@ impl VehicleJourneyAppend {
                             .map(|ec| ec.stop_point_ref.clone())
                     })
             }),
+            started: !recorded_call_rows.is_empty()
+                || estimated_call_rows
+                    .first()
+                    .and_then(|ec| ec.aimed_arrival_time.or_else(|| ec.aimed_departure_time))
+                    .map(|dt| dt > Utc::now())
+                    .unwrap_or(false),
+            finished: !estimated_call_rows.is_empty(),
         };
 
         Some(VehicleJourneyAppend {
@@ -372,21 +399,24 @@ impl VehicleJourneyAppend {
     }
 }
 
-pub fn vehicle_journeys(response: SiriETResponse) -> impl Iterator<Item = VehicleJourneyAppend> {
+pub fn vehicle_journeys(
+    response: SiriETResponse,
+    version: u32,
+) -> impl Iterator<Item = VehicleJourneyAppend> {
     response
         .siri
         .service_delivery
         .estimated_timetable_delivery
         .into_iter()
-        .flat_map(|timetable| {
+        .flat_map(move |timetable| {
             timetable
                 .estimated_journey_version_frame
                 .into_iter()
-                .flat_map(|frame| {
+                .flat_map(move |frame| {
                     frame
                         .estimated_vehicle_journey
                         .into_iter()
-                        .filter_map(VehicleJourneyAppend::new)
+                        .filter_map(move |journey| VehicleJourneyAppend::new(journey, version))
                 })
         })
 }
@@ -409,6 +439,7 @@ mod tests {
     fn vehicle_journy_row_roundtrip() -> anyhow::Result<()> {
         let row = VehicleJourneyRow {
             id: "id".to_string(),
+            version: 3,
             data_source: "data_source".to_string(),
             recorded_at_time: Default::default(),
             cancellation: true,
@@ -417,6 +448,8 @@ mod tests {
             direction_ref: "direction_ref".to_string(),
             destination_ref: Some("destination_ref".to_string()),
             origin_ref: Some("origin_ref".to_string()),
+            started: true,
+            finished: false,
         };
 
         let db = setup_db()?;
@@ -437,6 +470,7 @@ mod tests {
         let now: DateTime<FixedOffset> = Default::default();
         let row = RecordedCallRow {
             vehicle_journey_id: "vehicle_journey_id".to_string(),
+            version: 3,
             data_source: "data_source".to_string(),
             recorded_at_time: Default::default(),
             order: 1,
@@ -462,6 +496,7 @@ mod tests {
         let now: DateTime<FixedOffset> = Default::default();
         let row = EstimatedCallRow {
             vehicle_journey_id: "vehicle_journey_id".to_string(),
+            version: 2,
             data_source: "data_source".to_string(),
             recorded_at_time: Default::default(),
             order: 1,
